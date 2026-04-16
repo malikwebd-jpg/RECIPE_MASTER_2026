c68d84fa4bb019237093f243bfeace22b029e45e
malik12 <malik.webd@gmail.com>
2026-04-16 15:25:20 +0200

    route page.home

---
+++ app/controllers/pagesController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace App\Controllers\PagesController;
+
+
+use \PDO;
+use \App\Models\RecipesModel;
+
+
+function homeAction(PDO $conn)
+{
+    include_once __DIR__ . '/../models/recipesModel.php';
+    $randomRecipe = RecipesModel\findOneByRand($conn);
+
+
+    global $title, $content;
+    $title = "Home";
+    ob_start();
+    include __DIR__ . '/../views/home.php';
+    $content = ob_get_clean();
+}
